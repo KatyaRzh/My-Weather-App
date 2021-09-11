135aa6d233865547d49be89659af38f461af5d18
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-09-11 15:56:43 -0700

    Fixed the date format, rounded temperature and shorten #of days shown to <6

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ const formatDate = function (timestamp) {
 	return `${day} ${hours}:${minutes}`;
 };
 
+let formatDay = function (timestamp) {
+	let date = new Date(timestamp * 1000);
+	let day = date.getDay();
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+	return days[day];
+};
+
 // API call for a 7 day forecast
 let getForecast = function (response) {
 	console.log("getForecast function: ", response);
@@ -34,23 +42,30 @@ let displayForecast = function (response) {
 	// concatenating 2 strings
 	let forecastHTML = `<div class="row">`;
 	let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-	forecast.forEach(function (forecastDay) {
-		forecastHTML =
-			forecastHTML +
-			`
+	forecast.forEach(function (forecastDay, index) {
+		if (index < 6) {
+			forecastHTML =
+				forecastHTML +
+				`
 			<div class="col-2">
-				<div class="weather-forecast-date">${forecastDay.dt}</div>
+				<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+			
 			<img
 				src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
 				alt="sunny"
 				width="55px;"
 					/> 
 					<div class="weather-forecast-temperatures">
-						<span class="weather-forecast-temperature-max">${forecastDay.temp.max}</span> 
-						<span class="weather-forecast-temperature-min">${forecastDay.temp.min}</span>
+						<span class="weather-forecast-temperature-max">${Math.round(
+							forecastDay.temp.max
+						)}</span> 
+						<span class="weather-forecast-temperature-min">${Math.round(
+							forecastDay.temp.min
+						)}</span>
 					</div>
 			</div>
 	`;
+		}
 	});
 	forecastHTML = forecastHTML + `</div>`;
 	forecastElement.innerHTML = forecastHTML;
